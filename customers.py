ef322663e3ad68ff2cb6e8c8d0a52022f22dafd4
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-23 17:32:22 -0800

    further study

--- customers.py
+++ customers.py
@@ -6,4 +6,33 @@ class Customer(object):
 
     # TODO: need to implement this
 
-    def __init__
+    customer_list = {}
+
+    def __init__(self,first,last,email,password):
+
+        self.first = first
+        self.last = last
+        self.email = email
+        self.password = password
+
+
+    def __repr__(self):
+        """Convenience method to show information about melon in console."""
+
+        return ("<Customer: {}, {}, {}>".format(self.first, 
+            self.last, self.email))
+
+
+    def read_customer(filepath):
+
+        customers = {}
+
+        file = open(filepath)
+
+        for line in file:
+            customer = line.rstrip().split('|')
+            fis
+
+
+
+    customer_list = read_customer('customer.txt')
